f68819c86ace140d3d074c741c61880fe2c38749
Darius Daniel <dariusdaniel771@gmail.com>
2024-02-29 18:03:00 +0100

    Build the next and previous buttons (without behaviour)

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,9 +1,13 @@
 import RefreshTable from './RefreshTable';
+import Next from './NextButton';
+import Previous from './PreviousButton';
 
 export default function ToolBar() {
   return (
     <div className="w-65 m-auto mb-3">
       <RefreshTable />
+      <Previous />
+      <Next />
     </div>
   );
 }
